8b072c7f81c1600e471c6cff0df5e910d4808af0
Charles Liu <liuchangyang112@gmail.com>
2020-05-09 22:11:36 +0800

    Completed tentative Project model

--- projector_server/projects/models.py
+++ projector_server/projects/models.py
@@ -1,7 +1,46 @@
 from django.db import models
-
-# Create your models here.
+from django.utils.translation import gettext_lazy as _
 
 class Project(models.Model):
-    name = models.CharField(max_length=32)
-    pub_date = models.DateTimeField('Date Added')
+
+    # Main Fields
+    name = models.CharField(max_length=100)
+    created_at = models.DateTimeField('Created At', auto_now_add=True)
+    last_modified = models.DateTimeField('Last Modified', auto_now=True)
+    owner = models.CharField(max_length=100)
+    #owner = models.ForeignKey(User) #TODO: Change to this later after users are implemented
+    #members = models.ManyToManyField(User) #TODO: Add this in after users are implemented
+
+    #Choice fields
+    class ProjectCategory(models.TextChoices):
+        #Tentative. Add to this later when we know what categories to add
+        GENERAL = 'GEN', _('General')
+        TECH = 'TEC', _('Technology')
+        SERVICE = 'SVC', _('Service')
+        MEDIA = 'MED', _('Media')
+        GAMES = 'GAM', _('Games')
+    class ProjectStatus(models.TextChoices):
+        ONGOING = 'ON', _('Ongoing')
+        COMPLETED = 'CM', _('Completed')
+        CANCELLED = 'CA', _('Cancelled')
+    category = models.CharField(
+        max_length=3,
+        choices=ProjectCategory.choices,
+    )
+    status = models.CharField(
+        max_length=2,
+        choices=ProjectStatus.choices,
+        default=ProjectStatus.ONGOING,
+    )
+
+    #Optional project fields
+    logo = models.ImageField(upload_to='logos')
+    description = models.TextField(blank=True)
+    video = models.URLField(blank=True) #Youtube URL
+    deck = models.URLField(blank=True) #Google slides URL
+
+
+    #Methods
+    def __str__(self):
+        return self.name
+
