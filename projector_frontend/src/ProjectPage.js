fa512109cce10b057b2bb8abc49467a832a29525
ColeTrammer <cole.trammer@gmail.com>
2020-05-18 18:00:04 -0700

    Show project image, project video, and project slides on ProjectPage

--- projector_frontend/src/ProjectPage.js
+++ projector_frontend/src/ProjectPage.js
@@ -4,6 +4,7 @@ import React, { Component } from 'react';
 import { Badge, Button } from 'reactstrap';
 import * as Constants from './constants';
 import { Link } from 'react-router-dom';
+import YouTube from 'react-youtube';
 
 class ProjectPage extends Component {
     constructor(props) {
@@ -15,17 +16,17 @@ class ProjectPage extends Component {
 
     componentDidMount() {
         fetch(`${Constants.PROJECT_LIST_URL}${this.props.match.params.projectId}`)
-            .then(resp => {
+            .then((resp) => {
                 if (!resp.ok) {
                     // Invokes the .catch() handler
                     throw Error(resp.statusText);
                 }
                 return resp.json();
             })
-            .then(data => {
+            .then((data) => {
                 this.setState({ project: { failed: false, data: data } });
             })
-            .catch(error => {
+            .catch((error) => {
                 console.log(error);
                 this.setState({ project: { failed: true, error: error } });
             });
@@ -40,10 +41,31 @@ class ProjectPage extends Component {
         }
 
         if (project.failed) {
-            return <p>{"" + project.error}</p>;
+            return <p>{'' + project.error}</p>;
         }
 
         const { data } = project;
+
+        let videoId = null;
+        try {
+            const youtubeUrl = new URL(data.video);
+            const params = new URLSearchParams(youtubeUrl.search);
+            videoId = params.get('v');
+        } catch {
+            // There's no video, or the URL is malformed
+        }
+
+        let slidesEmbedUrl = null;
+        try {
+            const slidesUrl = new URL(data.deck);
+            const key = slidesUrl.pathname.replace(/\/presentation\/d\/(.*)\/.*/g, '$1');
+            slidesEmbedUrl = `https://docs.google.com/presentation/d/${key}/embed?start=false&loop=false&delayms=5000`;
+        } catch {
+            // There's no slides, or the URL is malformed
+        }
+
+        const logoUrl = data.logo !== Constants.DEFAULT_PROJECT_LOGO ? Constants.PROJECT_LOGO_PATH + data.logo : null;
+        console.log(logoUrl);
         return (
             <div className="ProjectPage-container">
                 <div className="ProjectPage-leftpanel">
@@ -53,18 +75,42 @@ class ProjectPage extends Component {
                     </p>
 
                     {/* TODO: Only one of these buttons should be visible at any time */}
-                    <Button
-                        color="primary"
-                        onClick={() => alert('Joining Project...')}
-                    >
+                    <Button color="primary" onClick={() => alert('Joining Project...')}>
                         Join {data.name}!
                     </Button>
-                    <Link className='btn btn-secondary' to={`/projects/${this.props.match.params.projectId}/edit`}>Edit</Link>
-
+                    <br />
+                    <Link
+                        style={{ marginTop: 16 }}
+                        className="btn btn-secondary"
+                        to={`/projects/${this.props.match.params.projectId}/edit`}
+                    >
+                        Edit
+                    </Link>
                 </div>
                 <div className="ProjectPage-rightpanel">
-                    <h3 className="text-center">{data.name}</h3>
-                    <p>{data.description}</p>
+                    <div
+                        className="ProjectPage-header-container"
+                        style={{
+                            backgroundImage: `${
+                                logoUrl
+                                    ? `linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.2)), url(${logoUrl})`
+                                    : 'linear-gradient(to bottom, #003b5c, #005587, #2774ae, #8bb8e8, #daebfe)'
+                            }`,
+                        }}
+                    >
+                        <h1 className="ProjectPage-title">{data.name}</h1>
+                    </div>
+                    <div className="ProjectPage-media-container">
+                        {videoId && (
+                            <YouTube videoId={videoId} containerClassName="ProjectPage-youtube-container" className="ProjectPage-youtube" />
+                        )}
+                        {slidesEmbedUrl && (
+                            <div className="ProjectPage-slides-container">
+                                <iframe src={slidesEmbedUrl} width="100%" height="100%" allowFullScreen={true} />
+                            </div>
+                        )}
+                    </div>
+                    <p style={{ marginTop: 16 }}>{data.description}</p>
                 </div>
             </div>
         );
