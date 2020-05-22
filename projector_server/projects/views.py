1f4b3ed8efc3b4e1595645f11ae9cc6a328c46a5
Charles Liu <liuchangyang112@gmail.com>
2020-05-22 21:15:03 +0800

    Simplified project views by changing to generic views

--- projector_server/projects/views.py
+++ projector_server/projects/views.py
@@ -4,67 +4,21 @@ from rest_framework.parsers import JSONParser, FormParser, MultiPartParser
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
+from rest_framework import generics
+
 from projects.models import Project
 from projects.serializers import ProjectSerializer
 
-# Create your views here.
 
-class BaseProjectView(APIView):
+class BaseProjectView(generics.ListCreateAPIView):
     parser_classes = [FormParser, MultiPartParser]
     permission_classes = [IsAuthenticatedOrReadOnly]
-
-    def get(self, request, *args, **kwargs):
-        """
-        List all projects
-        """
-        project_list = Project.objects.all()
-        serializer = ProjectSerializer(project_list, many=True)
-        return JsonResponse(serializer.data, safe=False)
-
-    def post(self, request, *args, **kwargs):
-        """
-        Create a new project
-        """
-        serializer = ProjectSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
 
 
-class DetailedProjectView(APIView):
+class DetailedProjectView(generics.RetrieveUpdateDestroyAPIView):
     parser_classes = [FormParser, MultiPartParser]
     permission_classes = [IsAuthenticatedOrReadOnly]
-
-    def get_object(self, pk, *args, **kwargs):
-        try:
-            return Project.objects.get(pk=pk)
-        except Project.DoesNotExist:
-            raise Http404
-
-    def get(self, request, pk, *args, **kwargs):
-        """
-        Get one particular project
-        """
-        project = self.get_object(pk)
-        serializer = ProjectSerializer(project)
-        return JsonResponse(serializer.data, safe=False)
-
-    def put(self, request, pk, *args, **kwargs):
-        """
-        Edit a project
-        """
-        project = self.get_object(pk)
-        serializer = ProjectSerializer(project, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
-
-    def delete(self, request, pk, *args, **kwargs):
-        """
-        Delete a project
-        """
-        project = self.get_object(pk)
-        project.delete()
-        return HttpResponse(status=204)
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
