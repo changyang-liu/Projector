2fb70437b8a7132e679338f4913e5d7ecc7e4a76
Charles Liu <liuchangyang112@gmail.com>
2020-05-22 18:39:53 +0800

    Incorporated token exchange and jwt refresh urls

--- projector_server/projector_server/urls.py
+++ projector_server/projector_server/urls.py
@@ -19,9 +19,13 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from rest_framework_simplejwt.views import TokenRefreshView
+
 urlpatterns = [
     path('projects/', include('projects.urls')),
+    path('social/google-oauth2/', include('oauth.urls')),
     path('admin/', admin.site.urls),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
 
 #Media URLs
