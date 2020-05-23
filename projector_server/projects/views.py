c7be38da038eb67a2f8a58e3496df7e5a423b9e2
Charles Liu <liuchangyang112@gmail.com>
2020-05-23 15:38:15 +0800

    Implemented IsOwnerOrReadOnly permission

--- projector_server/projects/views.py
+++ projector_server/projects/views.py
@@ -1,24 +1,32 @@
 from django.http import HttpResponse, JsonResponse, Http404
 
 from rest_framework.parsers import JSONParser, FormParser, MultiPartParser
-from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
-
 from rest_framework import generics
 
 from projects.models import Project
 from projects.serializers import ProjectSerializer
+from projects.permissions import IsOwnerOrReadOnly
 
 
 class BaseProjectView(generics.ListCreateAPIView):
+    """
+    View all projects (GET) or create a new project (POST)
+    """
     parser_classes = [FormParser, MultiPartParser]
     permission_classes = [IsAuthenticatedOrReadOnly]
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
 
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
 
 class DetailedProjectView(generics.RetrieveUpdateDestroyAPIView):
+    """
+    View a particular project (GET), edit a project (PUT) or delete a project (DELETE)
+    """
     parser_classes = [FormParser, MultiPartParser]
-    permission_classes = [IsAuthenticatedOrReadOnly]
+    permission_classes = [IsOwnerOrReadOnly]
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
