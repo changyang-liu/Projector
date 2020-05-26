7728a0c1fb156f55b885b305a4aa5695c0bce295
ColeTrammer <cole.trammer@gmail.com>
2020-05-25 21:32:12 -0700

    Add SERVER_GOOGLE_OAUTH_URL to constants.js

--- projector_frontend/src/constants.js
+++ projector_frontend/src/constants.js
@@ -7,4 +7,5 @@ export const MAX_USER_LENGTH = 100;
 export const MAX_PROJECT_LENGTH = 100;
 export const MAX_BLURB_LENGTH = 200;
 
+export const SERVER_GOOGLE_OAUTH_URL = 'http://localhost:8080/social/google-oauth2/';
 export const GOOGLE_CLIENT_ID = '701132387789-6s24lbf37aidsjmpqn04m1vfivpdp9rl.apps.googleusercontent.com';
