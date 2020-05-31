5d7f3c47d8621983f987c45fc46cc2d0ed465f25
Alex Yu <yu.alexanderx@gmail.com>
2020-05-31 00:02:59 -0700

    Fixed some warnings and errors
    I removed the CardText element because it wasn't doing anything to the styling, and it was raising warnings due to the mixing of in-line and block elements. I also added a check to make sure that an error isn't raised if someone who is not logged in tries to view a profile page as if they're logged in.

--- projector_frontend/src/UserPage.js
+++ projector_frontend/src/UserPage.js
@@ -1,8 +1,7 @@
 import React, {Component} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {
-    Card, CardImg, CardText, CardBody,
-    CardTitle, CardSubtitle, Button, NavLink
+    Card, CardImg, CardBody, Button, NavLink
 } from 'reactstrap';
 import * as Constants from './constants';
 //import UserForm from './components/user-form';
@@ -11,8 +10,8 @@ function Profile({person, projects}, visiting) {
     return (
         <div className="Profile">
             {!visiting ? 
-                <h1 className="Name">Hi, {person.name}</h1> : 
-                <h1 className="Name">{person.name}</h1>
+                <div>Hi, {person.name}</div> : 
+                <div id="Profile-name">{person.name}</div>
             }
             <p className="Email">{person.email}</p>
             <p className="Skills">My skills: {person.skills}</p>
@@ -46,8 +45,10 @@ class UserPage extends Component {
         let userToGet;
         if(this.props.visiting)
             userToGet = this.props.match.params.userId;
-        else
+        else if(this.props.user)
             userToGet = this.props.user.id;
+        else
+            return;
         fetch(`${Constants.USER_LIST_URL}${userToGet}`)
             .then((resp) => {
                 if (!resp.ok) {
@@ -92,15 +93,13 @@ class UserPage extends Component {
                 <Card style={{width: "80%", height: "80%"}} className="mx-auto my-auto">
                     <CardImg top width="100%" style={{maxHeight: 250, maxWidth: 250}} src={this.state.person.image} alt="Profile Picture"/>
                     <CardBody>
-                        <CardText>
-                            <Profile person={this.state.person} visiting={this.props.visiting}/>
-                            {!this.props.visiting && this.state.incomplete && (
-                                <div>
-                                    <h5>Your profile seems incomplete!</h5>
-                                    <h6>Please complete your profile below:</h6>
-                                </div>
-                            )}
-                        </CardText>
+                        <Profile person={this.state.person} visiting={this.props.visiting}/>
+                        {!this.props.visiting && this.state.incomplete && (
+                            <div>
+                                <h5>Your profile seems incomplete!</h5>
+                                <h6>Please complete your profile below:</h6>
+                            </div>
+                        )}
                         { !this.props.visiting && (
                             <Button><NavLink href="/profile/edit" style={{color: 'white', textDecoration: 'none'}}>
                                 Edit Profile
